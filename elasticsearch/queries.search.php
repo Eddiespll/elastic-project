b814671736840ab7c9bbbdbe5c96db9504170460
= <=>
2022-04-11 14:43:26 -0300

    Ajustes

--- elasticsearch/queries.search.php
+++ elasticsearch/queries.search.php
@@ -10,20 +10,20 @@ $queries = array(
 
 		            "must"=> [
 
-                		'multi_match' => [
-								"query" => $search,
-								"fields" => ["title", "main_content", "conteudo_pdf"],
-								"fuzziness" => 1,
+		                'multi_match' => [
+		                    "query" => $search,
+		                    "fields" => ["title", "main_content","conteudo_pdf"],
+		                    "fuzziness" => 1,
 							"prefix_length"=>3,
-                    		"analyzer" => "analyzer_brazilian",
-	                	]
+		                    "analyzer" => "analyzer_brazilian",
+		                ]
 		            ],
 
 		            "should"=> [
 
 		                'multi_match' => [
 		                    "query" => $search,
-		                    "fields" => ["title", "main_content", "conteudo_pdf"],
+		                    "fields" => ["title", "main_content","conteudo_pdf"],
 		                    "type"=>'phrase',
 		                    "slop"=>2,
 		                    "analyzer" => "analyzer_brazilian",
@@ -42,30 +42,32 @@ $queries = array(
 	),
 
 
-	'highlight' => array(
+   'highlight' => array(
 
-		"max_analyzed_offset" => 1000000,
-    	"pre_tags" => ["<b>"],
-		"post_tags" => ["</b>"],
-		"order"=> "score",
-    	"fields"=> [
+   		"max_analyzed_offset" => 1000000,
+   	    "pre_tags" => ["<b>"],
+   		"post_tags" => ["</b>"],
+    	"order"=> "score",
+        "fields"=> [
       		"main_content"=> [
-	 			"type"=> "fvh",
-	    		"fragment_size"=> 300,
-	    		"no_match_size" => 300,
-	    		"number_of_fragments"=> 1,
-	    		"highlight_query"=> [
-					"bool"=> [
-						"must"=> [
-	         				"match"=> [
-		           				"main_content"=> [
-	             					"query"=> $search,
-	             					"fuzziness" => 1,
-	             					"prefix_length"=>3,
-	             					"analyzer" => "analyzer_brazilian",
-		           				]
-	         				]
-	       				],
+      			"type"=> "fvh",
+        		"fragment_size"=> 300,
+        	//"fragment_offset" => 100,
+        		"no_match_size" => 300,
+        		"number_of_fragments"=> 1,
+        		"highlight_query"=> [
+          			"bool"=> [
+           				"must"=> [
+              				"match"=> [
+                				"main_content"=> [
+                  					"query"=> $search,
+                  					"fuzziness" => 1,
+                  					"prefix_length"=>3,
+                  					"analyzer" => "analyzer_brazilian",
+                				]
+              				]
+            			],
+            
 	            		"should"=> [
 		                	"match_phrase"=> [
 		                		"main_content"=> [
@@ -76,10 +78,42 @@ $queries = array(
 	                			]
 	              			]
 	            		],
-						"minimum_should_match"=> 0
-					]
-	        	]
-       		],
-		]		
-	),
+            			"minimum_should_match"=> 0
+            		]
+            	]
+            ],
+
+      		"conteudo_pdf"=> [
+      			"type"=> "fvh",
+        		"fragment_size"=> 300,
+        	//"fragment_offset" => 100,
+        		//"no_match_size" => 250,
+        		"number_of_fragments"=> 1,
+        		"highlight_query"=> [
+          			"bool"=> [
+           				"must"=> [
+              				"match"=> [
+                				"conteudo_pdf"=> [
+                  					"query"=> $search,
+                				]
+              				]
+            			],
+            
+	            	"should"=> [
+	                	"match_phrase"=> [
+	                		"conteudo_pdf"=> [
+	                  			"query"=> $search,
+	                  			"slop"=> 1,
+	                  			"boost"=> 10.0
+                			]
+              			]
+            		],
+            		
+            		"minimum_should_match"=> 0
+            		]
+            	]
+            ]
+
+        ]
+    ),
 );
